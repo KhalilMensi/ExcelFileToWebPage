65ac624c86f3252eef041820e15e5849ec807a4d
Khalil Mensi <mensikhalil@gmail.com>
2021-07-08 10:01:14 +0100

    Ajoutez des fichiers projet.

---
+++ ExcelFileToWebPage/Controllers/StudentsController.cs
@@ -0,0 +1,85 @@
+﻿using ExcelDataReader;
+using ExcelFileToWebPage.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelFileToWebPage.Controllers
+{
+	public class StudentsController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index(List<Student> students = null)
+		{
+			students = students == null ? new List<Student>() : students;
+			ViewBag.FileName = "Choose file";
+			return View(students);
+		}
+
+		[HttpPost]
+		public IActionResult Index(IFormFile file,[FromServices] IWebHostEnvironment hostingEnvironment,string FileName)
+		{
+			if (file != null)
+			{
+				string filename = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+				using (FileStream fileStream = System.IO.File.Create(filename))
+				{
+					file.CopyTo(fileStream);
+					fileStream.Flush();
+				}
+
+				var students = this.GetStudentList(file.FileName);
+
+				ViewBag.FileName = file.FileName;
+				ViewBag.File = file;
+
+				return View(students);
+			}
+			else if(file == null && FileName != "Choose file")
+			{
+				string filename = $"{hostingEnvironment.WebRootPath}\\files\\{FileName}";
+
+				var students = this.GetStudentList(FileName);
+
+				ViewBag.FileName = FileName;
+
+				return View(students);
+			}
+			else
+			{
+				ViewBag.FileName = "Choose file";
+				return View(new List<Student>());
+			}
+		}
+
+		private List<Student> GetStudentList(string fName)
+		{
+			List<Student> students = new List<Student>();
+			var fileName = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files"}" + "\\" + fName;
+
+			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+			using (var stream = System.IO.File.Open(fileName,FileMode.Open,FileAccess.Read))
+			{
+				 using(var reader = ExcelReaderFactory.CreateReader(stream))
+				{
+					while (reader.Read())
+					{
+						students.Add(new Student()
+						{
+						   Name = reader.GetValue(0).ToString(),
+						   Email = reader.GetValue(1).ToString(),
+						   Tel = reader.GetValue(2).ToString()
+						});
+					}
+				}
+			}
+			return students;
+		}
+	}
+}
---
+++ ExcelFileToWebPage/Models/DBConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelFileToWebPage.Models
+{
+	public class DBConnection
+	{
+        //static string DbConnnectionString = @"workstation id=Portail.mssql.somee.com;packet size=4096;user id=KhalilMensi_SQLLogin_1;pwd=3234ys9719;data source=Portail.mssql.somee.com;persist security info=False;initial catalog=Portail";
+        static string DbConnnectionString = @"Data Source=DESKTOP-A5NHULB\SQLEXPRESS;Initial Catalog=FSM;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";        //Create a database
+
+        public static void CreateDatabase()
+        {
+            SqlConnection myConn = new SqlConnection(@"Data Source=DESKTOP-A5NHULB\SQLEXPRESS;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            string str = "IF(NOT EXISTS (SELECT 'FSM' FROM master.dbo.sysdatabases WHERE ('[' + name + ']' = 'FSM' OR name = 'FSM'))) Create DATABASE FSM";
+
+            SqlCommand myCommand = new SqlCommand(str, myConn);
+            try
+            {
+                myConn.Open();
+                myCommand.ExecuteNonQuery();
+            }
+            catch (Exception e) { }
+        }
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(DbConnnectionString);
+        }
+    }
+}
